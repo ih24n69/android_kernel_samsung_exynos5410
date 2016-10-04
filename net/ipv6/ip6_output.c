79f579228cdb235a4236d1abc4f472dae8656bba
krasCGQ <albertibrahimtch@outlook.com>
2016-10-04 19:22:22 +0800

    net: ipv6: Fix 'too many definitions' compile errors

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1213,29 +1213,6 @@ static void ip6_append_data_mtu(unsigned int *mtu,
 	}
 }
 
-static void ip6_append_data_mtu(int *mtu,
-				int *maxfraglen,
-				unsigned int fragheaderlen,
-				struct sk_buff *skb,
-				struct rt6_info *rt)
-{
-	if (!(rt->dst.flags & DST_XFRM_TUNNEL)) {
-		if (skb == NULL) {
-			/* first fragment, reserve header_len */
-			*mtu = *mtu - rt->dst.header_len;
-
-		} else {
-			/*
-			 * this fragment is not first, the headers
-			 * space is regarded as data space.
-			 */
-			*mtu = dst_mtu(rt->dst.path);
-		}
-		*maxfraglen = ((*mtu - fragheaderlen) & ~7)
-			      + fragheaderlen - sizeof(struct frag_hdr);
-	}
-}
-
 int ip6_append_data(struct sock *sk, int getfrag(void *from, char *to,
 	int offset, int len, int odd, struct sk_buff *skb),
 	void *from, int length, int transhdrlen,
